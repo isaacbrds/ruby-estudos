cefbcc50ed71a6c6455eac6a82294e40a7477204
isaac <isaac.brigido@gmail.com>
2024-04-23 21:23:30 -0300

    feat: aprofundando nas associacoes

--- rails/2-model/model/db/schema.rb
+++ rails/2-model/model/db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_232450) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_001548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "biographs", force: :cascade do |t|
+    t.string "content"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_biographs_on_author_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,4 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_232450) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "biographs", "authors"
+  add_foreign_key "books", "authors"
 end
--- rails/2-model/model/db/seeds.rb
+++ rails/2-model/model/db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+10.times do |i|
+    Product.create(name: Faker::Commerce.product_name)
+end
