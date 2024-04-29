0c97ef52e6b79a0bb6b66e9330fe5b7bed2933b2
isaac <isaac.brigido@gmail.com>
2024-04-29 09:45:29 -0300

    feat: API, JWT e Swagger

--- rails/8-api/storebook/app/controllers/api/v1/authors_controller.rb
+++ rails/8-api/storebook/app/controllers/api/v1/authors_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AuthorsController < ApplicationController
+  before_action :authenticate_request
   before_action :set_author, only: %i[ show update destroy ]
 
   # GET /authors
@@ -39,6 +40,25 @@ class Api::V1::AuthorsController < ApplicationController
   end
 
   private
+    
+    def authenticate_request
+      token = request.headers['Authorization']
+      if token.present?
+        begin
+          decoded_token = JsonWebTokenService.decode(token.split(' ').last)
+          user_id = decoded_token['user_id']
+          @current_user = User.find_by(id: user_id)
+          unless @current_user
+            render json: { error: 'Not Authorized' }, status: :unauthorized
+          end  
+        rescue => exception
+          render json: { error: 'Invalid Token'}, status: :unauthorized
+        end
+      else
+        render json: { error: 'Missing Authorization Header'}, status: :unauthorized
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_author
       @author = Author.find_by(id: params[:id])
